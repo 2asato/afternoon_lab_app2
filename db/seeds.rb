38c1b4bf56ef3ce9d67a5aaf8f72ed04fb129f3d
aaronsato <2asato@gmail.com>
2018-03-20 00:16:27 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create([{ title: "Galaxy S6", price: 500.00}, { title: "iphone 7", price: 600.00}])
+
+Review.create(rating: 7, content: "could be better", product_id: 1)
+
+Review.create(rating: 6, content: "wish i had an iphone", product_id: 1)
+
+Review.create(rating: 10, content: "glad i have an iphone", product_id: 2)
